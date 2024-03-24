b03d69cbc67e70e35f5469d8e911e3fc0b898c9e
vikas <rawatvikas689@gmail.com>
2024-03-24 23:48:54 +0530

    commit

--- src/CommonFiles/Navigation.jsx
+++ src/CommonFiles/Navigation.jsx
@@ -1,11 +1,25 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Shop from "../Pages/Shop";
+// import Shop from "../Pages/Shop";
+import {
+  RiHammerFill,
+  RiMenu2Fill,
+  RiMenu2Line,
+  RiMenu3Fill,
+  RiMenu3Line,
+  RiMenu4Fill,
+  RiMenu5Fill,
+  RiMenuLine,
+  RiMenuUnfoldFill,
+  RiSearch2Line,
+  RiShoppingCartLine,
+  RiUserLine,
+} from "react-icons/ri";
 function Navigation() {
   return (
     <>
       <div className="fixed top-0 left-0 right-0 w-full z-20">
-        <div className=" flex justify-between h-16 items-center bg-headerColor text-gray-400">
+        <div className="hidden lg:flex justify-between h-16 items-center bg-headerColor text-gray-400">
           <div>
             <ul className="flex items-center text-xs gap-4 ml-4">
               <li>
@@ -38,6 +52,23 @@ function Navigation() {
               <li>ACCOUNT</li>
             </ul>
           </div>
+        </div>
+        <div className=" lg:hidden justify-between h-16 items-center bg-headerColor text-gray-400 flex">
+          <ul>
+            <li className="flex gap-2 ml-4">
+              <img src="images/hamburger.svg" alt="Logo" className="w-8" />
+              <RiSearch2Line size={30} />
+            </li>
+          </ul>
+          <ul className="w-24">
+            <img src="images/daz3d.svg" alt="Logo" />
+          </ul>
+          <ul>
+            <li className="flex gap-2 mr-4">
+              <RiUserLine size={30} />
+              <RiShoppingCartLine size={30} />
+            </li>
+          </ul>
         </div>
       </div>
     </>
