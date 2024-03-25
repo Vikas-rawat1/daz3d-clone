13b6098a22f907560cc3cc93787bcd5551618281
vikas <rawatvikas689@gmail.com>
2024-03-26 00:08:39 +0530

    commit

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,12 +1,37 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import { RiSearch2Line, RiShoppingCartLine, RiUserLine } from "react-icons/ri";
+import {
+  RiArrowDropDownLine,
+  RiArrowDropRightLine,
+  RiCloseLine,
+  RiSearch2Line,
+  RiShoppingCartLine,
+  RiUserLine,
+} from "react-icons/ri";
+import Collapsible from "react-collapsible";
 function Navigation() {
   const [showMenu, setShowMenu] = useState(false);
+  const [isCollapsed, setCollapsed] = useState(true);
+  const [isSoftwareCollapsed, setSoftwareCollapsed] = useState(true);
+  const [isModelCollapsed, setModelCollapsed] = useState(true);
+  const [isCommunityCollapsed, setCommunityCollapsed] = useState(true);
   const toggleMenu = () => {
     setShowMenu(!showMenu);
   };
 
+  const toggleCollapse = () => {
+    setCollapsed(!isCollapsed);
+  };
+
+  const toggleSoftwareCollapsed = () => {
+    setSoftwareCollapsed(!isSoftwareCollapsed);
+  };
+  const toggleModelCollapsed = () => {
+    setModelCollapsed(!isModelCollapsed);
+  };
+  const toggleCommunityCollapsed = () => {
+    setCommunityCollapsed(!isCommunityCollapsed);
+  };
   return (
     <>
       <div className="fixed top-0 left-0 right-0 w-full z-20">
@@ -62,8 +87,97 @@ function Navigation() {
           </ul>
         </div>
         {showMenu && (
-          <div className="lg:hidden fixed top-0 left-0 h-full bg-gray-800 w-64">
-            {/* Add sidebar content here */}
+          <div className="lg:hidden fixed top-0 left-0 h-full bg-gray-800 w-11/12  ">
+            <ul className=" transition-transform duration-300 text-xm text-yellow-50 ">
+              <span>
+                <li className="flex items-center justify-between mt-4 ml-3">
+                  <RiCloseLine size={30} onClick={toggleMenu} />
+                  <div className="flex items-center">
+                    <input
+                      type="text"
+                      placeholder="Search Store"
+                      className="outline-none border-none rounded-sm h-6 w-48 text-black text-sm mr-2"
+                    />
+                    <RiSearch2Line className="absolute right-3  text-black" />
+                  </div>
+                </li>
+              </span>
+
+              <li className="mt-6 ml-4">SHOP</li>
+              <li className="mt-6 ml-4">
+                <Collapsible
+                  trigger={
+                    <div
+                      className="flex justify-between items-center"
+                      onClick={toggleSoftwareCollapsed}
+                    >
+                      3D SOFTWARE
+                      {isSoftwareCollapsed ? (
+                        <RiArrowDropRightLine size={30} />
+                      ) : (
+                        <RiArrowDropDownLine size={30} />
+                      )}
+                    </div>
+                  }
+                >
+                  <li className="mt-4">Daz Studio</li>
+                  <li className="mt-4">Install Manager</li>
+                  <li className="mt-4">Daz to Maya</li>
+                  <li className="mt-4">Daz to Blender</li>
+                  <li className="mt-4">Daz to Unreal</li>
+                  <li className="mt-4">Daz to Unity</li>
+                  <li className="mt-4">Daz to 3ds Max</li>
+                  <li className="mt-4">Daz to Cinema 4D</li>
+                </Collapsible>
+              </li>
+              <li className="mt-6 ml-4">
+                <Collapsible
+                  trigger={
+                    <div
+                      className="flex justify-between items-center"
+                      onClick={toggleModelCollapsed}
+                    >
+                      3D MODELS
+                      {isCollapsed ? (
+                        <RiArrowDropRightLine size={30} />
+                      ) : (
+                        <RiArrowDropDownLine size={30} />
+                      )}
+                    </div>
+                  }
+                >
+                  <li className="mt-4">Genesis 9</li>
+                  <li className="mt-4">Genesis 8.1</li>
+                  <li className="mt-4">Free 3D Models</li>
+                </Collapsible>
+              </li>
+              <li className="mt-6 ml-4">
+                <Collapsible
+                  trigger={
+                    <div
+                      className="flex justify-between items-center"
+                      onClick={toggleCommunityCollapsed}
+                    >
+                      COMMUNITY
+                      {isCollapsed ? (
+                        <RiArrowDropRightLine size={30} />
+                      ) : (
+                        <RiArrowDropDownLine size={30} />
+                      )}
+                    </div>
+                  }
+                >
+                  <li className="mt-4"></li>
+                  <li className="mt-4">Our Community </li>
+                  <li className="mt-4">Gallery</li>
+                  <li className="mt-4">Forums</li>
+                  <li className="mt-4">Blog</li>
+                  <li className="mt-4">Daz to Unity</li>
+                  <li className="mt-4">Blog</li>
+                  <li className="mt-4">Help </li>
+                </Collapsible>
+              </li>
+            </ul>
           </div>
         )}
       </div>
