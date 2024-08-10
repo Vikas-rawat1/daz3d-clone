9365b00039a2051e28e6267557d84fcf48ced6a1
vikas <rawatvikas689@gmail.com>
2024-08-10 11:19:33 +0530

    commit

--- src/Pages/Download.jsx
+++ src/Pages/Download.jsx
@@ -81,15 +81,15 @@ function Download() {
                 fill="#00e4bb"
                 points="461.35 83.06 366.02 83.06 496.54 276.14 366.02 476.5 461.35 476.5 591.87 276.14 461.35 83.06"
               ></polygon>
-            </svg>sdfsddsafsfgsdfg
-          </div>sdfsadfasdfasdffgsdfg
+            </svg>
+          </div>
           <div className="lg:w-4/5 lg:mt-8 lg:block lg:mb-10 w-72 flex gap-10 ml-6 items-center">
-            <imgzasdfsadf
-              src="images/cart.svg"dfds
-              alt="Cart"adfasdfasadfsadf
+            <imgz
+              src="images/cart.svg"
+              alt="Cart"
               className="lg:w-10 w-10 lg:mb-2"
-            />asdfasdfadfsdfsad
-            <div>sfasdfadsfasdf
+            />
+            <div>
               <p className="lg:text-2xl text-xl font-bold ">Daz store</p>
               <p className="lg:text-lg text-lg ">
                 Ready-made 3D models and kits for characters, environments &
