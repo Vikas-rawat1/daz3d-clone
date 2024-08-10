f40e66491209561da29fc8c9721a8e7a67597cd2
vikas <rawatvikas689@gmail.com>
2024-08-10 11:22:41 +0530

    commit

--- src/Pages/Download.jsx
+++ src/Pages/Download.jsx
@@ -94,7 +94,7 @@ function Download() {
               <p className="lg:text-lg text-lg ">
                 Ready-made 3D models and kits for characters, environments &
                 props
-              </p>
+              </p>adfsdaf
             </div>
           </div>
           <div className="lg:w-4/5 lg:mt-8 lg:block lg:mb-10  w-72 flex gap-10 ml-6 mt-6 items-center">
