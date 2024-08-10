0264608ada765b06762e0f67c57b4bb2e444f6eb
vikas <rawatvikas689@gmail.com>
2024-08-10 11:13:56 +0530

    commit

--- src/Pages/Download.jsx
+++ src/Pages/Download.jsx
@@ -80,7 +80,7 @@ function Download() {
                 class="chev chev3"
                 fill="#00e4bb"
                 points="461.35 83.06 366.02 83.06 496.54 276.14 366.02 476.5 461.35 476.5 591.87 276.14 461.35 83.06"
-              ></polygon>
+              ></polygon>sdfsdf
             </svg>
           </div>
           <div className="lg:w-4/5 lg:mt-8 lg:block lg:mb-10 w-72 flex gap-10 ml-6 items-center">
